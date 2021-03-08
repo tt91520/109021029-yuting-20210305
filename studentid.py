f4e83e71b83aeb4f8f99c2e1eeda145c4a6ca045
tt91520 <109021029@live.asia.edu.tw>
2021-03-08 12:32:36 +0800

    github

---
+++ studentid.py
@@ -0,0 +1,7 @@
+import random
+studentid = ["109","108","107","106","105"]
+print(random.choice(studentid))
+
+from random import choice
+studentid = ["109","108","107","106","105"]
+print(choice(studentid))
